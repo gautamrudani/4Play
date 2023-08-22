2020c5b1201f940842680d4998040061a369835d
Gautam RudaNi <gautamrudani1@gmail.com>
2023-08-22 09:32:46 +0530

    add search baar

--- src/component/maincontainer/SearchBar.jsx
+++ src/component/maincontainer/SearchBar.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 import styles from '../maincontainer/searchbar.module.scss';
+import Search from '../../assets/images/search.svg';
+import Refresh from '../../assets/images/Refresh.svg';
 
 const SearchBar = () =>{
     return (
      
             <div className={styles.searchbar}>
-                    <div className={styles}></div>
-                    <input type="text" placeholder='Find your music' />
-                    <button>Refresh</button> 
+                    <div className={styles.searchInput}>
+                        <img src={Search} alt="" />
+                        <input type="text" placeholder='Find your music' />
+                    </div>
+                    <div className={styles.refreshbox}>
+                        <button><img src={Search} alt="" /> Refresh</button> 
+                    </div>
             </div>
        
     ) ;
